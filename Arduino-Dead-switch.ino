58add5ab8a002baf6b4c084491ea1d3107ff5d43
KrystofCL <Krystof.Hava@hotmail.com>
2023-07-12 16:25:49 +0200

    Add buzzer

--- Arduino-Dead-switch.ino
+++ Arduino-Dead-switch.ino
@@ -1,9 +1,11 @@
-void(* Stop) (void) = 0;
+void(* Stop) (void) = 0; //Function at adress 0, used to restart the Arduino
 
 #pragma region Variables
+/*Used pins*/
 const int LED_PIN = 3;
 const int BUTTON_PIN = 10;
 const int RELAY_PIN = 2;
+const int BUZZER_PIN = 6;
 
 bool running;
 bool ready;
@@ -11,43 +13,41 @@ bool ready;
 int lastTime;
 int deltaTime;
 
-/*ALL TIMES ARE IN MILIECONDS*/
-int buttonTime; //Time untin next check in request
-const int BUTTON_INTERVAL = 1000;
-const int BUTTON_TIME_WINDOW = 10000; //How mutch time is for pressing the button
+/*ALL TIMES ARE IN MILLISECONDS*/
+int buttonTime;
+const int BUTTON_INTERVAL = 2000; //How often is check-in required
+const int BUTTON_TIME_WINDOW = 5000; //How much time is for pressing the button
 int buttonHoldTime;
-const int BUTTON_HOLD_LIMIT = 2000;
+const int BUTTON_HOLD_LIMIT = 500; //How long are you allowed to hold the button
 
 int ledTime;
-const int LED_INTERVAL = 500;
+const int LED_INTERVAL = 500; //Half time of LED cycle
+
+int buzzerTime;
+const int BUZZER_INTERVAL = 500; //Half time of buzzer cycle
+const int BUZZER_DELAY = 1000; //How long will the buzzer wait before joining the blinking LED
+
+const int BUZZER_TONE = 10; //Tone set in Hz
 #pragma endregion
 
 void setup() {
   #pragma region Pinmodes
-  pinMode(LED_PIN,OUTPUT);
-  pinMode(BUTTON_PIN,INPUT);
-  pinMode(RELAY_PIN,OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
   #pragma endregion
-
   digitalWrite(LED_PIN, 1);
-  Serial.begin(9600); //Allows communication
 }
 
 void loop() {
   digitalWrite(RELAY_PIN,running);
   deltaTime = millis() - lastTime; //Detlat time calcilations
   lastTime = millis();             //
-  
-  if(running && ready) {
-    CheckButton();
-    BeepBuzzer();
-  }
-  else if(!digitalRead(BUTTON_PIN) && !ready) {
-    ready = true;
-  }
-  else if(digitalRead(BUTTON_PIN) && ready) {
-    running = true;
-  }
+
+  if(running && ready)                        CheckButton();
+  else if(!digitalRead(BUTTON_PIN) && !ready) ready = true;
+  else if( digitalRead(BUTTON_PIN) &&  ready) running = true;
 }
 
 void BlinkLed(int interval) {
@@ -57,8 +57,11 @@ void BlinkLed(int interval) {
   if(ledTime > interval) ledTime = -interval;
 }
 
-void BeepBuzzer() {
-  //TODO: Add buzzer logic!
+void BeepBuzzer(int interval, int _tone) {
+  if(buzzerTime > 0) tone(BUZZER_PIN, _tone);
+  else               noTone(BUZZER_PIN);
+  buzzerTime += deltaTime;
+  if(buzzerTime > interval) buzzerTime = -interval;
 }
 
 void CheckButton() {
@@ -68,6 +71,7 @@ void CheckButton() {
     if(buttonTime < -BUTTON_TIME_WINDOW) Stop();
     else {
       BlinkLed(LED_INTERVAL);
+      if(buttonTime <= -BUZZER_DELAY) BeepBuzzer(BUZZER_INTERVAL, BUZZER_TONE);
     }
   }
 
@@ -76,7 +80,9 @@ void CheckButton() {
     if(buttonHoldTime < BUTTON_HOLD_LIMIT) {
       buttonTime = BUTTON_INTERVAL;
       digitalWrite(LED_PIN, 0);
+      noTone(BUZZER_PIN);
       ledTime = 0;
+      buzzerTime = 0;
     }
     else Stop();
   }
